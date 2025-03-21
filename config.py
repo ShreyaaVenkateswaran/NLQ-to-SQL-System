c6f58ce5b5b076b36dd61c6a9e3ec813bdfb3c6f
Shreyaa Venkateswaran <126344266+ShreyaaVenkateswaran@users.noreply.github.com>
2025-03-21 20:28:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "********",
+    "database": "sql_ai_db"
+}
+
+GROQ_API_KEY = "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"
