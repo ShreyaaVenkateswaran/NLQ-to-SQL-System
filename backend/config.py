81fdc3d67a008e20d4a98d5d833196113e2f7c95
Shreyaa Venkateswaran <126344266+ShreyaaVenkateswaran@users.noreply.github.com>
2025-05-29 13:24:21 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,7 @@
 DB_CONFIG = {
     "host": "localhost",
-    "user": "root",
-    "password": "Krishna3*",
+    "user": "",
+    "password": "",
     "database": "sql_ai_db"
 }
 
-GROQ_API_KEY = "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"
